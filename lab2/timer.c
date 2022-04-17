269d885ed50ea87c4096dc072020c97c4503f47d
Ze Diogo <up202003529@edu.fe.up.pt>
2022-04-17 18:45:35 +0100

    lab2 first func - timer_test_read_config

--- lab2/timer.c
+++ lab2/timer.c
@@ -3,7 +3,67 @@
 
 #include <stdint.h>
 
+#include <minix/syslib.h>
+
 #include "i8254.h"
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+
+  if (timer < 0 || timer > 2 || st == NULL) return 1;
+  
+  uint8_t RB_CMD = TIMER_RB_CMD | TIMER_RB_SEL(timer) | TIMER_RB_COUNT_;
+
+  if (sys_outb(0x43, RB_CMD)) return 1;
+
+  if (util_sys_inb(TIMER_0 + timer, st)) return 1;
+
+  return 0;
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+  
+  union timer_status_field_val conf;
+
+  uint8_t in_mode;
+
+  switch (field) {
+
+    case tsf_all:
+      conf.byte = st;
+      break;
+
+    case tsf_initial:
+
+      in_mode = (st & (BIT(5) | BIT(4))) >> 4; 
+
+      switch(in_mode) {
+
+        case 0: conf.in_mode = INVAL_val; break;
+        case 1: conf.in_mode = LSB_only; break;
+        case 2: conf.in_mode = MSB_only; break;
+        case 3: conf.in_mode = MSB_after_LSB; break;
+        default: return 1;
+      }
+      break;
+
+    case tsf_mode:
+      conf.count_mode = (st & (BIT(3) | BIT(2) | BIT(1))) >> 1;
+
+      if (conf.count_mode == 6) conf.count_mode = 2;
+      if (conf.count_mode == 7) conf.count_mode = 3;
+
+      break;
+    
+    case tsf_base: 
+
+      conf.count_mode = (st & BIT(0));
+      break;
+
+    default: return 1;
+  }
+
+  return timer_print_config(timer, field, conf);
+}
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   /* To be implemented by the students */
@@ -31,17 +91,6 @@ void (timer_int_handler)() {
   printf("%s is not yet implemented!\n", __func__);
 }
 
-int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
-}
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
-}
--- lab2/utils.c
+++ lab2/utils.c
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#include <minix/syslib.h>
+
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
@@ -17,8 +19,12 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
+  uint32_t tmp;
 
-  return 1;
+  if (sys_inb(port, &tmp)) return 1;
+
+  *value = (uint8_t) tmp;
+
+  return 0;
 }
